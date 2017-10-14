19ada98e74380352bbb43278cc8dd24836d39cd5
Ajay Kumar Yadav <ajya@microsoft.com>
2017-10-14 23:46:36 +0530

    learn vue

---
+++ todo_application/src/app.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        todo: null,
+        todos: []
+    },
+    methods: {
+        add(todo) {
+            if(!this.todoIsValid) {
+                return;
+            }
+            
+            this.todos.push(todo);
+            this.todo = null;
+        },
+    },
+    computed: {
+        todoIsValid() {
+           return !!this.todo; 
+        }
+    }
+})
