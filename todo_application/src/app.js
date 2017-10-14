ec8d40bb13bcf20697351d4d90a16fa97e2bcc78
Ajay Kumar Yadav <ajya@microsoft.com>
2017-10-15 00:48:51 +0530

    added delete and edit functionality

--- todo_application/src/app.js
+++ todo_application/src/app.js
@@ -3,21 +3,52 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         todo: null,
-        todos: []
+        editTodo: null,
+        todos: [{
+            content: 'The First Todo',
+            finished: false,
+            isEdited: false
+        }]
     },
     methods: {
         add(todo) {
             if(!this.todoIsValid) {
                 return;
             }
-            
-            this.todos.push(todo);
+
+            this.todos.push({
+                content:todo,
+                finished: false
+            });
             this.todo = null;
         },
+        edit(todo) {
+            this.editTodo = todo.content;
+            todo.isEdited=true;
+        },
+        remove(todo) {
+            this.todos = this.todos.filter((item) => {
+                return item != todo
+            });
+        },
+        update(todo) {
+            if(!this.editTodoIsValid) {
+                return;
+            }
+
+            todo.content = this.editTodo;
+            todo.isEdited = false;
+        }
     },
     computed: {
         todoIsValid() {
            return !!this.todo; 
+        },
+        editTodoIsValid() {
+            return !!this.editTodo;
+        },
+        isBeingEdited() {
+            return this.todos.filter(todo => todo.isEdited).length > 0;
         }
     }
 })
